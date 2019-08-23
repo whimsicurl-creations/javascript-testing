efa5a615bac987b7b802dc591fdaa7c5d1640f48
Bethany <blmeier@gmail.com>
2019-08-23 15:11:31 -0400

    multiple parallax sections

--- another-parallax/main.js
+++ another-parallax/main.js
@@ -1,8 +1,20 @@
 const parallax = document.getElementById('parallax');
+const parallaxTwo = document.getElementById('parallaxTwo');
+const parallaxThree = document.getElementById('parallaxThree');
 
 window.addEventListener('scroll', function() {
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + 'px';
 });
 
-console.log('linked');
+window.addEventListener('scroll', function() {
+    let offset = window.pageYOffset;
+    // Have to keep rate of scrolling low to prevent overlap
+    parallaxTwo.style.backgroundPositionY = offset * 0.3 + 'px';
+});
+
+window.addEventListener('scroll', function() {
+    let offset = window.pageYOffset;
+    // See note above
+    parallaxThree.style.backgroundPositionY = offset * 0.2 + 'px';
+});
